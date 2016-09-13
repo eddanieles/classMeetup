f0f0214a9afaeef2e279a7c0af23284418cd1698
Kevin Chambers <kbchambers2@gmail.com>
2016-09-13 15:45:52 -0400

    Save local changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
+import base from '../config/base'
+import axios from 'axios'
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listings: []
+    }
+    this.returnListings = this.returnListings.bind(this)
+  }
+
+
+returnListings() {
+  axios.get("https://api.meetup.com/2/events?key=ABDE12456AB2324445&group_urlname=ny-tech&sign=true")
+  .then(function(response){
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+     }
+
 
   render() {
 
     return (
       <div>
-      hello from home
+        {this.returnListings()}
       </div>
-    );
+
+    )
   }
 }
+
 
 export default Home;
