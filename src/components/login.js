53d7a5ff0e3a9070e2123fc57b552851fa588c61
eddanieles <ed.danieles@yahoo.com>
2016-09-13 16:24:41 -0400

    updated api

--- src/components/login.js
+++ src/components/login.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react'
 import base from '../config/base'
 
-
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.whatevs = this.whatevs.bind(this);
-  }
-  handleClick(event) {
-    event.preventDefault();
-     base.authWithOAuthPopup('github', this.whatevs)
+ constructor(props) {
+   super(props);
+   this.handleClick = this.handleClick.bind(this);
+   this.authHandler = this.authHandler.bind(this);
+ }
+ handleClick(event) {
+   event.preventDefault();
+    base.authWithOAuthPopup('github', this.authHandler)
+    // rebase method that authenticates a user using an
+    // OAuth popup. Also, takes in an Auth provider and handler
 
-  }
+ }
 
-  whatevs(err, user) {
-      console.log("does this work");
-  }
+ authHandler(err, user) {
+     console.log("does this work");
 
-  render() {
+     this.props.history.replaceState(null, "/home")
+     // history is an object of the DOM. replaceState is a function
+     // that modifies the current history instead of creating a new one
+ }
 
-    return (
-      <button className="loginBtn" onClick={this.handleClick}>Sign in with Github</button>
-    );
-  }
+ render() {
+   console.log(this.props)
+
+   return (
+     <button className="loginBtn" onClick={this.handleClick}>Sign in with Github</button>
+   );
+ }
 }
 
 export default Login;
