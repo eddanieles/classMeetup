22d114abd71500706ec73668728176792be606a8
lakeblack <lake.black@ymail.com>
2016-09-13 13:07:45 -0400

    routes are set up

---
+++ src/config/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import App from '../components/App';
+import Home from '../components/home';
+import Details from '../components/details';
+import Login from '../components/login';
+
+
+const routes = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Login}/>
+      <Route path="home" component={Home}/>
+       <Route path="/details" component={Details}/>
+    </Route>
+  </Router>
+)
+
+export default routes;
