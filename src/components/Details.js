1bbe1df6a4e9cf059ab0dc5c305029d2c3222aa3
eddanieles <ed.danieles@yahoo.com>
2016-09-15 17:59:30 -0400

    added RSVP

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 import CommentBox from './CommentBox'
 import Directions from './Directions'
 import moment from 'moment'
+import axios from 'axios'
 
 class Details extends Component {
-
+  onClickRSVP(event_id, rsvp) {
+      axios.post(`https://api.meetup.com/2/rsvp?event_id=${this.props.clickedMeetup.id}&rsvp=yes&access_token=${localStorage.token}`)
+     .then(function(response) {
+       return response;
+     })
+   }
   render() {
     console.log(this.props.clickedMeetup);
     return (
@@ -13,6 +19,7 @@ class Details extends Component {
          <p>{this.props.clickedMeetup.venue.address_1}</p>
          <p>{this.props.clickedMeetup.venue.city}</p>
          <p>{moment(this.props.clickedMeetup.time).format ('MMMM Do')}</p>
+         <button type="submit" onClick={this.onClickRSVP.bind(this)} className="btn btn-primary">RSVP</button>
         <CommentBox />
         <Directions lat={this.props.clickedMeetup.venue.lat} lon={this.props.clickedMeetup.venue.lon}/>
 
