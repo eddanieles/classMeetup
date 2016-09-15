46ffbf6db3a9221856449eb9f51a026bc4a39d6a
josephrstraub <josephrstraub@gmail.com>
2016-09-15 14:17:00 -0400

    pass user to different components

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,44 @@ import '../App.css';
 import {Link} from 'react-router'
 
 class App extends Component {
+  constructor(props){
+     super(props);
+     this.state = {
+       currentUser: ''
+     }
+     this.setUser = this.setUser.bind(this);
+   }
+   setUser(userFromLogin){
+     this.setState({currentUser: userFromLogin})
+     if (localStorage.user) {
+       return
+     } else {
+       localStorage.setItem('user', userFromLogin);
+     }
+   }
   render() {
     return (
       <div className="App">
         <nav className="navbar navbar-inverse">
-      <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <Link to='/home' className="navbar-brand" href="#">Meetup</Link>
-              <ul className="nav navbar-nav">
-                  <li className="wishList">
-                      <Link to="/details">previousMeetups</Link>
-                  </li>
-                  <li className="logout">
-                      <Link to="/">Logout</Link>
-                  </li>
+          <div className="container-fluid">
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <Link to='/home' className="navbar-brand" href="#">Meetup</Link>
+                  <ul className="nav navbar-nav">
+                      <li className="wishList">
+                          <Link to="/details">previousMeetups</Link>
+                      </li>
+                      <li className="logout">
+                          <Link to="/">Logout</Link>
+                      </li>
 
-              </ul>
+                  </ul>
+              </div>
           </div>
-
-      </div>
-  </nav>
-        {this.props.children}
+        </nav>
+        {React.cloneElement(this.props.children, {
+         currentUser: this.state.currentUser,
+         setUser: this.setUser
+        })}
       </div>
     );
   }
