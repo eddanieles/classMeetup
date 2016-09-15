87777bf77a19fbd120bb8aa366b4f398767e4dda
eddanieles <ed.danieles@yahoo.com>
2016-09-15 15:48:59 -0400

    added event details to details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CommentBox from './CommentBox'
 import Directions from './Directions'
 import SimpleMap from './simpleMap.js'
+import moment from 'moment'
 
 class Details extends Component {
 
@@ -9,7 +10,10 @@ class Details extends Component {
     console.log(this.props.clickedMeetup);
     return (
       <div>
-        hello from details
+        <h2>{this.props.clickedMeetup.group.name}</h2>
+         <p>{this.props.clickedMeetup.venue.address_1}</p>
+         <p>{this.props.clickedMeetup.venue.city}</p>
+         <p>{moment(this.props.clickedMeetup.time).format ('MMMM Do')}</p>
         <CommentBox />
         <SimpleMap />
       </div>
